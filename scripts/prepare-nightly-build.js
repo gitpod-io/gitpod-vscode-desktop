ea0099c514ec38489d4cf258e0b6250c9b21b294
jeanp413 <jeanp413@hotmail.com>
2022-05-25 23:02:35 -0500

    Add nightly github action

---
+++ scripts/prepare-nightly-build.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const argv = require('minimist')(process.argv.slice(2));
+
+const json = JSON.parse(fs.readFileSync('./package.json').toString());
+const stableVersion = json.version.match(/(\d+)\.(\d+)\.(\d+)/);
+const major = stableVersion[1];
+const minor = stableVersion[2];
+
+function prependZero(number) {
+    if (number > 99) {
+        throw 'Unexpected value to prepend with zero';
+    }
+    return `${number < 10 ? '0' : ''}${number}`;
+}
+
+// update name, publisher and description
+// calculate version
+let patch = argv['v'];
+if (typeof patch !== 'number') {
+    const date = new Date();
+    const month = date.getUTCMonth() + 1;
+    const day = date.getUTCDate();
+    const hours = date.getUTCHours();
+    patch = `${date.getUTCFullYear()}${prependZero(month)}${prependZero(day)}${prependZero(hours)}`;
+}
+
+// The stable version should always be <major>.<minor_even_number>.patch
+// For the nightly build, we keep the major, make the minor an odd number with +1, and add the timestamp as a patch.
+const insiderPackageJson = Object.assign(json, {
+    version: `${major}.${Number(minor) + 1}.${patch}`
+});
+
+fs.writeFileSync('./package.insiders.json', JSON.stringify(insiderPackageJson, undefined, '\t'));
