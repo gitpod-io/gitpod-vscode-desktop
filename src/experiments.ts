8b096e367450cf3342da6adefe620dd45e937eeb
Jean Pierre <jeanp413@hotmail.com>
2022-08-17 14:17:33 -0500

    Add experiments using ConfigCat (#15)

---
+++ src/experiments.ts
@@ -0,0 +1,90 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Gitpod. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import * as configcat from 'configcat-node';
+import * as configcatcommon from 'configcat-common';
+import * as semver from 'semver';
+import Log from './common/logger';
+
+const EXPERTIMENTAL_SETTINGS = [
+    'gitpod.remote.useLocalApp'
+];
+
+export class ExperimentalSettings {
+    private configcatClient: configcatcommon.IConfigCatClient;
+    private extensionVersion: semver.SemVer;
+
+    constructor(key: string, extensionVersion: string, private logger: Log) {
+        this.configcatClient = configcat.createClientWithLazyLoad(key, {
+            logger: {
+                debug(): void { },
+                log(): void { },
+                info(): void { },
+                warn(message: string): void { logger.warn(`ConfigCat: ${message}`); },
+                error(message: string): void { logger.error(`ConfigCat: ${message}`); }
+            },
+            requestTimeoutMs: 1500,
+            cacheTimeToLiveSeconds: 60
+        });
+        this.extensionVersion = new semver.SemVer(extensionVersion);
+    }
+
+    async get<T>(key: string, userId?: string): Promise<T | undefined> {
+        const config = vscode.workspace.getConfiguration('gitpod');
+        const values = config.inspect<T>(key.substring('gitpod.'.length));
+        if (!values || !EXPERTIMENTAL_SETTINGS.includes(key)) {
+            this.logger.error(`Cannot get invalid experimental setting '${key}'`);
+            return values?.globalValue ?? values?.defaultValue;
+        }
+        if (this.isPreRelease()) {
+            // PreRelease versions always have experiments enabled by default
+            return values.globalValue ?? values.defaultValue;
+        }
+        if (values.globalValue !== undefined) {
+            // User setting have priority over configcat so return early
+            return values.globalValue;
+        }
+
+        const user = userId ? new configcatcommon.User(userId) : undefined;
+        const configcatKey = key.replace(/\./g, '_'); // '.' are not allowed in configcat
+        const experimentValue = (await this.configcatClient.getValueAsync(configcatKey, undefined, user)) as T | undefined;
+
+        return experimentValue ?? values.defaultValue;
+    }
+
+    async inspect<T>(key: string, userId?: string): Promise<{ key: string; defaultValue?: T; globalValue?: T; experimentValue?: T } | undefined> {
+        const config = vscode.workspace.getConfiguration('gitpod');
+        const values = config.inspect<T>(key.substring('gitpod.'.length));
+        if (!values || !EXPERTIMENTAL_SETTINGS.includes(key)) {
+            this.logger.error(`Cannot inspect invalid experimental setting '${key}'`);
+            return values;
+        }
+
+        const user = userId ? new configcatcommon.User(userId) : undefined;
+        const configcatKey = key.replace(/\./g, '_'); // '.' are not allowed in configcat
+        const experimentValue = (await this.configcatClient.getValueAsync(configcatKey, undefined, user)) as T | undefined;
+
+        return { key, defaultValue: values.defaultValue, globalValue: values.globalValue, experimentValue };
+    }
+
+    isUserOverride(key: string): boolean {
+        const config = vscode.workspace.getConfiguration('gitpod');
+        const values = config.inspect(key.substring('gitpod.'.length));
+        return values?.globalValue !== undefined;
+    }
+
+    forceRefreshAsync(): Promise<void> {
+        return this.configcatClient.forceRefreshAsync();
+    }
+
+    private isPreRelease() {
+        return this.extensionVersion.minor % 2 === 1;
+    }
+
+    dispose(): void {
+        this.configcatClient.dispose();
+    }
+}
--- src/remoteConnector.ts
+++ src/remoteConnector.ts
@@ -29,6 +29,7 @@ import { getGitpodVersion, isFeatureSupported } from './featureSupport';
 import SSHConfiguration from './ssh/sshConfig';
 import { isWindows } from './common/platform';
 import { untildify } from './common/files';
+import { ExperimentalSettings } from './experiments';
 
 interface SSHConnectionParams {
 	workspaceId: string;
@@ -118,7 +119,12 @@ export default class RemoteConnector extends Disposable {
 
 	private heartbeatManager: HeartbeatManager | undefined;
 
-	constructor(private readonly context: vscode.ExtensionContext, private readonly logger: Log, private readonly telemetry: TelemetryReporter) {
+	constructor(
+		private readonly context: vscode.ExtensionContext,
+		private readonly experiments: ExperimentalSettings,
+		private readonly logger: Log,
+		private readonly telemetry: TelemetryReporter
+	) {
 		super();
 
 		if (isGitpodRemoteWindow(context)) {
@@ -770,11 +776,13 @@ export default class RemoteConnector extends Disposable {
 
 		this.logger.info('Opening Gitpod workspace', uri.toString());
 
+		const userOverride = this.experiments.isUserOverride('gitpod.remote.useLocalApp');
 		const forceUseLocalApp = vscode.workspace.getConfiguration('gitpod').get<boolean>('remote.useLocalApp')!;
+		// const forceUseLocalApp = (await this.experiments.get<boolean>('gitpod.remote.useLocalApp', session.account.id))!;
 		let sshDestination: string | undefined;
 		if (!forceUseLocalApp) {
 			try {
-				this.telemetry.sendRawTelemetryEvent('vscode_desktop_ssh', { kind: 'gateway', status: 'connecting', ...params, gitpodVersion: gitpodVersion.raw });
+				this.telemetry.sendRawTelemetryEvent('vscode_desktop_ssh', { kind: 'gateway', status: 'connecting', ...params, gitpodVersion: gitpodVersion.raw, userOverride });
 
 				const { destination, password } = await this.getWorkspaceSSHDestination(session.accessToken, params);
 				sshDestination = destination;
@@ -783,9 +791,9 @@ export default class RemoteConnector extends Disposable {
 					await this.showSSHPasswordModal(password, params);
 				}
 
-				this.telemetry.sendRawTelemetryEvent('vscode_desktop_ssh', { kind: 'gateway', status: 'connected', ...params, gitpodVersion: gitpodVersion.raw });
+				this.telemetry.sendRawTelemetryEvent('vscode_desktop_ssh', { kind: 'gateway', status: 'connected', ...params, gitpodVersion: gitpodVersion.raw, userOverride });
 			} catch (e) {
-				this.telemetry.sendRawTelemetryEvent('vscode_desktop_ssh', { kind: 'gateway', status: 'failed', reason: e.toString(), ...params, gitpodVersion: gitpodVersion.raw });
+				this.telemetry.sendRawTelemetryEvent('vscode_desktop_ssh', { kind: 'gateway', status: 'failed', reason: e.toString(), ...params, gitpodVersion: gitpodVersion.raw, userOverride });
 				if (e instanceof NoSSHGatewayError) {
 					this.logger.error('No SSH gateway:', e);
 					vscode.window.showWarningMessage(`${e.host} does not support [direct SSH access](https://github.com/gitpod-io/gitpod/blob/main/install/installer/docs/workspace-ssh-access.md), connecting via the deprecated SSH tunnel over WebSocket.`);
@@ -817,15 +825,15 @@ export default class RemoteConnector extends Disposable {
 		let localAppSSHConfigPath: string | undefined;
 		if (!usingSSHGateway) {
 			try {
-				this.telemetry.sendRawTelemetryEvent('vscode_desktop_ssh', { kind: 'local-app', status: 'connecting', ...params, gitpodVersion: gitpodVersion.raw });
+				this.telemetry.sendRawTelemetryEvent('vscode_desktop_ssh', { kind: 'local-app', status: 'connecting', ...params, gitpodVersion: gitpodVersion.raw, userOverride });
 
 				const localAppDestData = await this.getWorkspaceLocalAppSSHDestination(params);
 				sshDestination = localAppDestData.localAppSSHDest;
 				localAppSSHConfigPath = localAppDestData.localAppSSHConfigPath;
 
-				this.telemetry.sendRawTelemetryEvent('vscode_desktop_ssh', { kind: 'local-app', status: 'connected', ...params, gitpodVersion: gitpodVersion.raw });
+				this.telemetry.sendRawTelemetryEvent('vscode_desktop_ssh', { kind: 'local-app', status: 'connected', ...params, gitpodVersion: gitpodVersion.raw, userOverride });
 			} catch (e) {
-				this.telemetry.sendRawTelemetryEvent('vscode_desktop_ssh', { kind: 'local-app', status: 'failed', reason: e.toString(), ...params, gitpodVersion: gitpodVersion.raw });
+				this.telemetry.sendRawTelemetryEvent('vscode_desktop_ssh', { kind: 'local-app', status: 'failed', reason: e.toString(), ...params, gitpodVersion: gitpodVersion.raw, userOverride });
 				this.logger.error(`Failed to connect ${params.workspaceId} Gitpod workspace:`, e);
 				if (e instanceof LocalAppError) {
 					const seeLogs = 'See Logs';
