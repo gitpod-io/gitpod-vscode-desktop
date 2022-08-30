63a4f3c99b76755a6aaa1a4560c021702fb9ad59
Filip Troníček <filip@gitpod.io>
2022-08-30 20:18:13 +0200

    Set default config for stable builds during CI (#18)
    Co-authored-by: jeanp413 <jeanp413@hotmail.com>

---
+++ scripts/prepare-release-build.js
@@ -0,0 +1,17 @@
+//@ts-check
+
+const fs = require("fs");
+
+const releasePackageJson = JSON.parse(fs.readFileSync('./package.json').toString());
+
+const releaseDefaultConfig = new Map([
+    ["gitpod.remote.useLocalApp", true],
+    ["gitpod.remote.syncExtensions", false],
+]);
+
+const gitpodConfig = releasePackageJson.contributes.configuration.find(e => e.title.toLowerCase() === 'gitpod');
+for (const [setting, value] of releaseDefaultConfig) {
+    gitpodConfig.properties[setting].default = value;
+}
+
+fs.writeFileSync('./package.release.json', JSON.stringify(releasePackageJson, undefined, '\t') + '\n');
