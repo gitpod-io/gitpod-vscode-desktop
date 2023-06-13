5286b8e125a89c02a4a045272ea0bee99b9ccd86
hwen <mhqnwt@gmail.com>
2023-06-13 16:09:42 +0800

    :lipstick:

--- src/services/localSSHMetrics.ts
+++ src/services/localSSHMetrics.ts
@@ -21,17 +21,17 @@ export class LocalSSHMetricsReporter {
         if (status === 'success') {
             failureCode = 'None';
         }
-        return addCounter(this.metricsHost, 'vscode_desktop_local_ssh_config_count', { status, failure_code: failureCode ?? 'Unknown' }, 1, this.logService);
+        return addCounter(this.metricsHost, 'vscode_desktop_local_ssh_config_total', { status, failure_code: failureCode ?? 'Unknown' }, 1, this.logService);
     }
 
     reportPingExtensionStatus(status: 'succeed' | 'failure') {
-        return addCounter(this.metricsHost, 'vscode_desktop_ping_extension_server_count', { status }, 1, this.logService);
+        return addCounter(this.metricsHost, 'vscode_desktop_ping_extension_server_total', { status }, 1, this.logService);
     }
 
     reportConnectionStatus(phase: 'connected' | 'connecting' | 'failed', failureCode?: string) {
         if (phase === 'connecting' || phase === 'connected') {
             failureCode = 'None';
         }
-        return addCounter(this.metricsHost, 'vscode_desktop_local_ssh_count', { phase, failure_code: failureCode ?? 'Unknown' }, 1, this.logService);
+        return addCounter(this.metricsHost, 'vscode_desktop_local_ssh_total', { phase, failure_code: failureCode ?? 'Unknown' }, 1, this.logService);
     }
 }
